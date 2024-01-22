104143c7c4b251318109de970562d7e99a8c0294
Carlos Eduardo <carlosrossy@bemol.com.br>
2024-01-22 19:34:08 -0400

    fix: url api

--- src/global/config/api.ts
+++ src/global/config/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.1.8:3333/development",
+  baseURL: "https://omni-connect-service.onrender.com/development",
 });
 export default api;
